128d77c7f3f64241f49ac238775f396a964ad6f5
goalkeeperfyc <664947387@qq.com>
2018-10-02 03:04:42 +0800

    test multiprocessing module

---
+++ test_multiprocessing2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  2 03:00:02 2018
+
+@author: fangyucheng
+"""
+
+from multiprocessing import Process
+import os
+
+def info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+
+def f(name):
+    info('function f')
+    print('hello', name)
+
+if __name__ == '__main__':
+    info('main line')
+    p = Process(target=f, args=('bob',))
+    p.start()
+    p.join()
